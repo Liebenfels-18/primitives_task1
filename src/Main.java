18e506d90398f8f78e5a065481d2d94ff39793ed
Nikita Yarych <nik.yarych2001@yandex.ru>
2023-09-24 17:07:47 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 13_676;
+        int quantity = 20;
+
+        int miles = cost / quantity;
+        System.out.println(miles);
+
+    }
+}
